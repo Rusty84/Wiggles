fc36b8baa9e4cbbab05d0e79154193da88ba30b8
Rusty84 <russ.morley@gmail.com>
2025-10-04 12:47:07 +0100

    Added test tags for UI tests.

--- app/src/main/java/dev/spikeysanju/wiggles/view/Home.kt
+++ app/src/main/java/dev/spikeysanju/wiggles/view/Home.kt
@@ -19,8 +19,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import dev.spikeysanju.wiggles.component.ItemDogCard
@@ -29,7 +31,7 @@ import dev.spikeysanju.wiggles.model.Dog
 
 @Composable
 fun Home(navController: NavHostController, dogList: List<Dog>, toggleTheme: () -> Unit) {
-    LazyColumn {
+    LazyColumn(modifier = Modifier.testTag("dogsList")) {
         item {
             TopBar(
                 onToggle = {
